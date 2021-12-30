1fc841f0e7dd795d3b41430ca73e7448195a59a0
HyeokJoon Kong <orijoon98@gmail.com>
2021-12-30 17:57:22 +0900

    dto 수정중

--- src/main/java/com/fleta/employee/model/dto/Employee.java
+++ src/main/java/com/fleta/employee/model/dto/Employee.java
@@ -1,9 +1,36 @@
 package com.fleta.employee.model.dto;
 
 import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.util.Date;
 
 @Data
 public class Employee {
-    private int id;
+    private String id;
+    private String password;
+    private String picture;
     private String name;
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private Date joinDate;
+    private String country;
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private Date birthday;
+    private String gender;
+    private String zipCode;
+    private String address;
+    private String addressDetail;
+    private String number;
+    private String phoneNumber;
+    private String email;
+    private String department;
+    private String rank;
+    private String occupation;
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private Date leaveDate;
+    private String bank;
+    private String bankNumber;
+    private String bankName;
+    private String note;
+    private String grade;
 }
