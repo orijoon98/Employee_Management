c1460301c9c16fac1e834df6458771d08b306fe6
HyeokJoon Kong <orijoon98@gmail.com>
2022-01-03 15:22:58 +0900

    dto 수정완료

--- src/main/java/com/fleta/employee/controller/EmployeeController.java
+++ src/main/java/com/fleta/employee/controller/EmployeeController.java
@@ -4,6 +4,7 @@ import com.fleta.employee.model.dto.Employee;
 import com.fleta.employee.service.EmployeeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -13,7 +14,7 @@ import java.util.List;
 public class EmployeeController {
     private final EmployeeService employeeService;
 
-    @GetMapping("/employee/all")
+    @GetMapping("api/employee/all")
     public List<Employee> getAllEmployee() {
         return employeeService.getAllEmployee();
     }
--- src/main/java/com/fleta/employee/model/dto/Employee.java
+++ src/main/java/com/fleta/employee/model/dto/Employee.java
@@ -1,36 +1,33 @@
 package com.fleta.employee.model.dto;
 
 import lombok.Data;
-import org.springframework.format.annotation.DateTimeFormat;
 
 import java.util.Date;
 
 @Data
 public class Employee {
-    private String id;
-    private String password;
-    private String picture;
-    private String name;
-    @DateTimeFormat(pattern = "yyyy-MM-dd")
-    private Date joinDate;
-    private String country;
-    @DateTimeFormat(pattern = "yyyy-MM-dd")
-    private Date birthday;
-    private String gender;
-    private String zipCode;
-    private String address;
-    private String addressDetail;
-    private String number;
-    private String phoneNumber;
-    private String email;
-    private String department;
-    private String rank;
-    private String occupation;
-    @DateTimeFormat(pattern = "yyyy-MM-dd")
-    private Date leaveDate;
-    private String bank;
-    private String bankNumber;
-    private String bankName;
-    private String note;
-    private String grade;
+    private String 사번;
+    private String 비밀번호;
+    private String 사진;
+    private String 이름;
+    private Date 입사년월일;
+    private String 국적;
+    private Date 생년월일;
+    private String 성별;
+    private String 우편번호;
+    private String 주소;
+    private String 상세주소;
+    private String 전화번호;
+    private String 휴대폰번호;
+    private String 이메일;
+    private String 부서;
+    private String 직급;
+    private String 직종;
+    private Date 퇴사년월일;
+    private String 급여이체은행;
+    private String 계좌번호;
+    private String 예금주;
+    private String 비고;
+    private String 등급;
 }
+
